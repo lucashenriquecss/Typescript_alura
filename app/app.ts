622a7d24164420167410abfdada0f52c3d6722bf
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2022-05-08 08:33:45 -0300

    Adicionando tipos

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,10 @@
-import { Negociacao } from './models/negociacao.js';
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(),10,100);
-console.log(negociacao.volume);
+const controller = new NegociacaoController();
+
+const form = document.querySelector('.form')//Pegando o formulario para adicionar
+
+form.addEventListener('submit', event => {
+    event.preventDefault(); // Cancelanddo evento padrao 
+    controller.adicionar();  
+})
---
+++ app/controllers/readme.md
@@ -0,0 +1,2 @@
+## Classe que vai servir como ponte entre interação de usuário na pagina e criação de modelo
+## Vai pegar os dados do formulario e criar o modelo
